86f21944fc49b1df604103ba775261c7e452d354
intrnl <intrnl28+git@gmail.com>
2022-10-16 12:55:20 +0700

    fix: don't hide battledia quest desc

--- src/elements/x-quest-info-window.js
+++ src/elements/x-quest-info-window.js
@@ -129,14 +129,7 @@ class QuestInfoWindowController extends WindowController {
 		this.#maxPlayersIcon.src = `assets/ui/players_${data.max_players}.webp`;
 		this.#maxPlayersIcon.title = t(`ui.quest_info.max_players_info`, { value: data.max_players });
 		this.#questName.textContent = t(`${type}.${data.id}.name`);
-
-		if (type !== 'battledias') {
-			this.#questDescription.style.display = '';
-			this.#questDescription.textContent = t(`${type}.${data.id}.description`);
-		}
-		else {
-			this.#questDescription.style.display = 'none';
-		}
+		this.#questDescription.textContent = t(`${type}.${data.id}.description`);
 
 		if (data.ranks) {
 			const select = this.#rankSelect;
