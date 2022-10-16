c8e29fb7c1d74674874e26126ca6b4fb64a11d30
intrnl <intrnl28+git@gmail.com>
2022-10-16 11:23:04 +0700

    deploy: e3af8e69ba1efb3c56128c7d6027a2ab2d67a9a6

--- _locales/chunk-ZXMCPFRJ.js
+++
@@ -1,2 +0,0 @@
-function s(t){return s[t]||(s[t]=new Intl.NumberFormat(t))}function i(t,o,e){return s(t).format(o-e)}function x(t,o,e,r,c){if({}.hasOwnProperty.call(r,t))return r[t];o&&(t-=o);var n=e(t,c);return n in r?r[n]:r.other}var p=t=>{let o=String(t).split("."),e=!o[1];return t==1&&e?"one":"other"};var u=p;export{i as a,x as b,u as c};
-//# sourceMappingURL=chunk-ZXMCPFRJ.js.map
